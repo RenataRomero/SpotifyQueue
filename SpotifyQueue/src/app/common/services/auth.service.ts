7618f41a3f69d8886c57a63a5da43f11559c3657
Alonso L <me@on247.me>
2019-12-09 00:18:20 -0600

    Recuperar tokens desde servidor

--- SpotifyQueue/src/app/common/services/auth.service.ts
+++ SpotifyQueue/src/app/common/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -17,5 +17,8 @@ export class AuthService {
     const url = `${environment.url}login`;
     return this.httpClient.get(url).toPromise();
   }
-  
+  getTokens():Observable<any> {
+    const url = `${environment.url}tokens`;
+    return this.httpClient.get(url);
+  }
 }
